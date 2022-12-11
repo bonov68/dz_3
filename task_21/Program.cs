1a209cf9cceae081df91784260ffa0e22b6f186f
bonov68 <example@mail.ru>
2022-12-11 10:21:17 +0300

    task_21 done

---
+++ task_21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ведите первую А координату");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите вторую А координату");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите третью А координату");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите первую В координату");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите вторую В координату");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите третью В координату");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine(Math.Round(length, 3));
